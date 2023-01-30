7a7e5de1073d06540d4f7c19c031551315ebaee3
Alan <ale@kea.dk>
2023-01-30 16:46:19 +0100

    subtle

--- js/produkt.js
+++ js/produkt.js
@@ -7,16 +7,11 @@ function getProduct() {
     .then(visProdukt);
 }
 
-/* async function getData() {
- const resp = await fetch(url);
- const data = await resp.json();
- visProdukt(data);
-}  */
-
 function visProdukt(produkt) {
   document.querySelector(".purchaseBox h3").textContent = produkt.productdisplayname;
   document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
   document.querySelector("img").alt = produkt.productdisplayname;
+  // etc. med de øvrige data
 }
 
 getProduct();
